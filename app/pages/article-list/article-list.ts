38bdb2069ff6628b74b4939f6184df95a2d1a5d2
Lukasz Kaniowski <lukasz.kaniowski@gmail.com>
2016-03-24 18:30:52 +0100

    sample article list with cards

--- app/pages/article-list/article-list.ts
+++ app/pages/article-list/article-list.ts
@@ -5,4 +5,18 @@ import {Page} from 'ionic-angular';
     templateUrl: 'build/pages/article-list/article-list.html',
 })
 export class ArticleListPage {
+
+    articles = [
+        {
+            title: 'Title',
+            image: 'http://lorempixel.com/400/200/',
+            description: "Some short description"
+        },
+        {
+            title: 'Some other stuff',
+            image: 'http://lorempixel.com/400/200/',
+            description: "And some longer description. And some longer description"
+        }
+    ];
+    
 }
