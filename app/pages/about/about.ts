6723ba9f738775950a04268c31ac6b889de9b9b0
Lukasz Kaniowski <lukasz.kaniowski@gmail.com>
2016-03-24 17:06:32 +0100

    cleaning pages to represent domain better

---
+++ app/pages/about/about.ts
@@ -0,0 +1,8 @@
+import {Page} from 'ionic-angular';
+
+
+@Page({
+    templateUrl: 'build/pages/about/about.html',
+})
+export class AboutPage {
+}
---
+++ app/pages/article-list/article-list.ts
@@ -0,0 +1,8 @@
+import {Page} from 'ionic-angular';
+
+
+@Page({
+    templateUrl: 'build/pages/article-list/article-list.html',
+})
+export class ArticleListPage {
+}
